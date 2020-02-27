41f87adf86f7feafac25144b7d0b2d2a03868858
DaltonVasquez <32403797+DaltonVasquez@users.noreply.github.com>
2020-02-26 20:37:40 -0600

    Added a scaled interface that uses block size as the scaling measurement.

--- App.py
+++ App.py
@@ -3,27 +3,34 @@ import pygame
 
 pygame.init()
 
-WIDTH = 800
-HEIGHT = 850
+BLOCK_SIZE = 25
 
-BLOCK_COLOR = (255,0,0)             #Red
-BACKGROUND_COLOR = (0,0,0)          #Black
-FOREGROUND_COLOR = (255,255,255)    #White
+win_width = 16*BLOCK_SIZE
+win_height = 20*BLOCK_SIZE
 
-block_pos = [400,300]
-block_size = 50
+play_width = int(BLOCK_SIZE*7)
+play_height = int(BLOCK_SIZE*15)
+play_posx = int(win_width/3)
+play_posy = int(win_height/10)
+
+BLOCK_COLOR = (255, 0, 0)  # Red
+BACKGROUND_COLOR = (0, 0, 0)  # Black
+FOREGROUND_COLOR = (255, 255, 255)  # White
+
+block_start_pos = [play_posx, play_posy+play_height-BLOCK_SIZE]
 
 pygame.time.Clock()
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((win_width, win_height))
 
 game_over = False
 
+
 def draw_play_area(frozen_blocks):
     screen.fill(BACKGROUND_COLOR)
-    pygame.draw.rect(screen, FOREGROUND_COLOR, (150, 50, 7*50, 15*50))
-    for x,y in frozen_blocks:
-        pygame.draw.rect(screen, BLOCK_COLOR, (frozen_blocks[x][0], frozen_blocks[y][0], block_size, block_size))
+    pygame.draw.rect(screen, FOREGROUND_COLOR, (play_posx, play_posy, play_width, play_height))
+    # for x in frozen_blocks:
+    #   pygame.draw.rect(screen, BLOCK_COLOR, (frozen_blocks[x][0], frozen_blocks[x][0], block_size, block_size))
 
 
 while not game_over:
@@ -37,12 +44,12 @@ while not game_over:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                block_pos[0] -= block_size
+                block_start_pos[0] -= BLOCK_SIZE
             elif event.key == pygame.K_RIGHT:
-                block_pos[0] += block_size
+                block_start_pos[0] += BLOCK_SIZE
         else:
             pass
 
-    pygame.draw.rect(screen, BLOCK_COLOR, (block_pos[0], block_pos[1], block_size, block_size))
+    pygame.draw.rect(screen, BLOCK_COLOR, (block_start_pos[0], block_start_pos[1], BLOCK_SIZE, BLOCK_SIZE))
 
     pygame.display.update()
